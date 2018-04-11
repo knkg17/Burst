7504ad177a3a8b2d4fafa55490f7acb3f088864e
knkg17 <kalleskaviar17@gmail.com>
2018-04-11 19:10:15 +0200

    .
    .

---
+++ Burst/Assets/Scripts/Gauge.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gauge : MonoBehaviour {
+	public Transform load;
+	public float width, height, maxWidth;
+	public float gaugeValue, maxGaugeValue;
+	// Use this for initialization
+	void Start () {
+		width = 0f;
+		height = 0.8f;
+		maxWidth = 0.98f;
+		load.localScale = new Vector3( width, height, 1f );
+		gaugeValue = 0f;
+		maxGaugeValue = 100f;
+	}
+
+	private float _charge = 1f;
+	// Update is called once per frame
+	void Update () {
+		if( _addWidth ) {
+			float add = Time.deltaTime / _chargeTime * _amount;
+			//Debug.Log( add.ToString( "0.00" ) );
+			_steps -= add;
+			gaugeValue += add;
+
+			UpdateWidth();
+			if( _steps <= 0f ) {
+				_addWidth = false;
+				_chargeTime = 0f;
+			}
+		}
+	}
+
+	private float _chargeAmount = 0f;
+	private float _chargeTime = 0f;
+	private float _previousGaugeValue;
+	private bool _addWidth = false;
+	private float _steps = 0f;
+	private float _amount;
+
+	public void Increase ( float val, float time ) {
+		_chargeTime = time;
+		_addWidth = true;
+		_steps = val;
+		_amount = val;
+	}
+
+	public float Subtract ( float val ) {
+		if(gaugeValue > 0 ) {
+			if( gaugeValue - val > 0 ) {
+				gaugeValue -= val;
+				UpdateWidth();
+				return val;
+			}
+			float v = gaugeValue;
+			gaugeValue = 0f;
+			UpdateWidth();
+			return v;
+		}
+		return -1;
+	}
+
+	private void UpdateWidth ( ) {
+		float newWidth = gaugeValue / maxGaugeValue;
+		if( gaugeValue >= maxGaugeValue )
+			newWidth = 1f;
+		width = maxWidth * newWidth;
+		load.localScale = new Vector3( width, height, 1f );
+	}
+
+	public float Emptying ( ) {
+		float v = gaugeValue;
+		gaugeValue = 0f;
+		UpdateWidth();
+		return v;
+	}
+}
---
+++ Burst/Assets/Scripts/GunDirection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunDirection : MonoBehaviour {
+	public Vector3 direction;
+	public Transform myTransform;
+	// Use this for initialization
+	void Start () {
+		_main = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetAngle();
+	}
+	private Camera _main;
+	private void SetAngle ( ) {
+		Vector3 aim = _main.ScreenToWorldPoint( Input.mousePosition );
+		float angle;
+		Vector2 a = new Vector2( myTransform.position.x, myTransform.position.y );
+		Vector2 b = new Vector2( aim.x, aim.y );
+		//Debug.Log( a.ToString() + ", " + b.ToString() );
+		float abangle = Vector2.Angle( a, b );
+		float baangle = Vector2.Angle( b, a );
+		float origoangle = Vector2.Angle( new Vector2(0f,0f), b );
+
+		//Debug.Log( "angles, ab: " + abangle.ToString( "00.00" + ", ba: " + baangle.ToString( "00.00" ) + ",  oa: " + origoangle.ToString( "00.00" ) ));
+		//angle = Vector2.Angle( ConvertV3( myTransform.position ), ConvertV3( aim ) );
+
+		angle = GetAngle( a, b );
+		/*
+		if( angle < 90f )
+			angle = 90f - angle;
+		else if( angle > 90f )
+			angle = angle - 90f;
+		else
+			angle = 0f;
+		//*/
+		//Debug.Log( aim.ToString() + ", " + myTransform.position.ToString() + " - " + angle.ToString("0000.0000") );
+		myTransform.eulerAngles = new Vector3( 0f, 0f, angle );
+	}
+
+	private float GetAngle(Vector2 a, Vector2 b ) {
+		float x = b.x - a.x;
+		float y = b.y - a.y;
+
+		float arctan = Mathf.Atan( x / y ) * Mathf.Rad2Deg;
+		//Debug.Log( arctan );
+		return arctan * -1f;
+	}
+
+	private Vector2 ConvertV3( Vector3 v ) {
+		return new Vector2( v.x, v.y );
+	}
+}
